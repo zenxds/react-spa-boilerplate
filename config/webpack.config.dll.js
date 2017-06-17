a50ea6ff983ea3fc26a65f02c039fd3c4a03bc34
zenxds <zenxds@gmail.com>
2017-06-18 00:32:15 +0800

    删除无用配置

--- config/webpack.config.dll.js
+++ config/webpack.config.dll.js
@@ -1,8 +1,6 @@
-// https://www.maizhiying.me/posts/2017/03/01/webpack-babel-ie8-support.html
 const path = require('path')
 const moment = require('moment')
 const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const dependencies = require('../package.json').dependencies
 
 module.exports = {
@@ -11,83 +9,13 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, '../build'),
-    publicPath: '/',
     filename: '[name].js'
-  },
-  module: {
-    rules: [{
-        test: /\.jsx?$/,
-        loader: ['babel-loader'],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf|svg)(\?[a-z0-9=\.]+)?$/,
-        use: 'url-loader?limit=10000'
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: 'url-loader?limit=8192&name=image/[hash].[ext]'
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract([
-          {
-            loader: 'css-loader',
-            options: {
-              minimize: true
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              config: {
-                path: 'config/postcss.config.js'
-              }
-            }
-          }
-        ])
-      },
-      {
-        test: /\.less$/,
-        use: ExtractTextPlugin.extract([
-          {
-            loader: 'css-loader',
-            options: {
-              minimize: true
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              config: {
-                path: 'config/postcss.config.js'
-              }
-            }
-          },
-          {
-            loader: 'less-loader',
-            options: {
-              relativeUrls: false
-            }
-          }
-        ])
-      }
-    ]
   },
   plugins: [
     new webpack.DllPlugin({
       path: path.join(__dirname, '../tmp', 'manifest.json'),
       name: '[name]'
     }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('prod')
-    }),
-    new ExtractTextPlugin({
-      disable: false,
-      allChunks: true,
-      filename: '[name].css'
-    }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.BannerPlugin(`${moment().format('YYYY-MM-DD HH:mm:ss')}`)
   ]
