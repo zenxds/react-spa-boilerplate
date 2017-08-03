2a100f80940e4466fe7e006fa4841ae247133f0f
zenxds <zenxds@gmail.com>
2017-08-03 14:17:37 +0800

    dashboard port

--- src/util/request.js
+++ src/util/request.js
@@ -1,11 +1,15 @@
 import fetch from 'isomorphic-fetch'
 
-export default (...args) => {
-  return fetch(...args).then((response) => {
+export default (input, options={}) => {
+  options = Object.assign({
+    credentials: "same-origin"
+  }, options)
+
+  return fetch(input, options).then((response) => {
     return response.json()
   }).then((response) => {
     if (response.success) {
-      return response.result
+      return response.data
     } else {
       throw new Error(response.msg || 'request error')
     }
