2c34a10bba742746a7319de29d61bd826c60ee48
zenxds <zenxds@gmail.com>
2019-04-22 19:32:37 +0800

    优化test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,12 @@
-test('application', () => {
-  expect(true).toBeTruthy()
+import { mount, shallow } from 'enzyme'
+import {
+  MemoryRouter as Router
+} from 'react-router-dom'
+
+import Header from '../src/component/Header'
+
+test('header', () => {
+  const wrapper = mount(<Router><Header /></Router>)
+
+  expect(wrapper.html()).toBeTruthy()
 })
